44dbff980ab0b5797aa82f9d4b95817527aaf7f0
lilian21-meet <lilian21@meet.mit.edu>
2019-07-25 07:25:20 -0400

    Stopping here

---
+++ lab 5.py
@@ -0,0 +1,24 @@
+Python 3.6.8 (default, Jan 14 2019, 11:02:34) 
+[GCC 8.0.1 20180414 (experimental) [trunk revision 259383]] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> one = [1,2,3,4]
+>>> two = [7,6,5,4]
+>>> three = ["y1","friends","fun"]
+>>> print(one+two)
+[1, 2, 3, 4, 7, 6, 5, 4]
+>>> print (one[3])
+4
+>>> one.remove(4)
+>>> print(one)
+[1, 2, 3]
+>>> one.append(4)
+>>> print(one)
+[1, 2, 3, 4]
+>>> one.pop(1)
+2
+>>> print(one)
+[1, 3, 4]
+>>> one.sort()
+>>> print(one)
+[1, 3, 4]
+>>> 
