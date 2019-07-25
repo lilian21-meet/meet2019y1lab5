b4cb2aed19bcc5d5b8515db06e72d4b6c04bfc06
lilian21-meet <lilian21@meet.mit.edu>
2019-07-25 05:29:50 -0400

    Stopping here

---
+++ lists.py
@@ -0,0 +1,25 @@
+Python 3.6.8 (default, Jan 14 2019, 11:02:34) 
+[GCC 8.0.1 20180414 (experimental) [trunk revision 259383]] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a={'she','sells','by','the','sea','shore'}
+>>> b="selfish shellfish"
+>>> c = {1, 1, 2, 3, 5, 8, 13}
+>>> b{3:4}
+SyntaxError: invalid syntax
+>>> a = ['she' ,'sells','sea','shells','by','the','sea','shore']
+>>> b = "selfish shellfish"
+>>> c = [1, 1, 2, 3, 5, 8, 13]
+>>> b[3:4]
+'f'
+>>> c[-2]
+8
+>>> c[:-2]
+[1, 1, 2, 3, 5]
+>>> a[2]
+'sea'
+>>> dogs = [dog,'poodle','boxer']
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    dogs = [dog,'poodle','boxer']
+NameError: name 'dog' is not defined
+>>> len
